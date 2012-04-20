7a5126687552eeac7f60ef4542268f674f6259cd
C. Scott Ananian <cscott@cscott.net>
2012-04-20 09:11:44 -0400

    Generate toolbar buttons programmatically.

--- index.js
+++ index.js
@@ -6,11 +6,56 @@
 define(['./src/dom', './hammer'], function(Dom, Hammer) {
     Dom.insertMeta(document);
 
+    // add toolbar buttons.
+    var toolbar_buttons = document.getElementById('toolbar_buttons');
+
+    var addButton = function(className) {
+        var span = document.createElement('span');
+        span.className = 'icon '+className;
+        toolbar_buttons.appendChild(span);
+        return span;
+    };
+    var undo = addButton('undo');
+    var redo = addButton('redo');
+    var hard = addButton('hard');
+    var soft = addButton('soft');
+
+    var addSwatch = function(colorName) {
+        var span = document.createElement('span');
+        span.className = 'swatch '+colorName;
+        var innerSpan = document.createElement('span');
+        span.appendChild(innerSpan);
+        toolbar_buttons.appendChild(span);
+        return span;
+    };
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo',
+                  'violet', 'white', 'black'];
+    var color_buttons = colors.map(function(c) { return addSwatch(c); });
+    color_buttons.forEach(function(cb, i) {
+        var colorName = colors[i];
+        cb.addEventListener('click', function() {
+            console.log(colorName, "button pressed");
+        }, false);
+    });
+
+    var addRange = function(id, min, max, value, step) {
+        var input = document.createElement('input');
+        input.type = 'range';
+        input.id = id;
+        input.min = min;
+        input.max = max;
+        input.value = value;
+        input.step = step;
+        toolbar_buttons.appendChild(input);
+        return input;
+    };
+    var size = addRange('size', 1, 40, 20, 1);
+    var opacity = addRange('opacity', 0, 1, 1, 'any');
+
     // allow dragging the toolbar left and right
     var toolbar = document.getElementById('toolbar');
-    var toolbar_buttons = document.getElementById('toolbar_buttons');
     var hammer = new Hammer(toolbar, {
-        prevent_default: true,
+        prevent_default: false,
         drag_vertical: false,
         transform: false
     });
