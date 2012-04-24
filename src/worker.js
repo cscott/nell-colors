d50583193a02622a691ac5b87ac782978c6a297b
C. Scott Ananian <cscott@cscott.net>
2012-04-23 22:03:53 -0400

    Update gh-pages branch to match master cf3627cccea4a16ef809369759d43c37870ad466

---
+++ src/worker.js
@@ -0,0 +1,36 @@
+/*jshint
+  eqeqeq:true, curly:true, latedef:true, newcap:true, undef:true,
+  trailing:true, es5:true
+ */
+/*global importScripts:false, require:false, console:true,
+         addEventListener:false, postMessage:false */
+
+// This module contains just the require.js thunks to use AMD w/in the web
+// worker environment.
+importScripts('../require.js');
+require.config({
+    paths: {
+        json: "../json",
+        text: "../text"
+    }
+});
+// compatibility thunk
+console = {
+    assert: function(condition, message) {
+        if (!condition) { throw new Error(message); }
+    },
+    log: function(varargs) {
+        var message = {
+            type: "debug",
+            args: Array.prototype.slice.call(arguments, 0)
+        };
+        postMessage(JSON.stringify(message));
+    }
+};
+console.warn = console.log;
+console.error = console.log;
+
+require(['./recogworker'], function(RecogWorker) {
+    RecogWorker.startup(addEventListener, postMessage);
+    postMessage(JSON.stringify({type:'ready'}));
+});
