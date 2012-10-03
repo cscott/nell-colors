21a447eda97bf1d74229469bd265f4dfa9df6dd0
C. Scott Ananian <cscott@cscott.net>
2012-10-03 16:50:04 -0400

    Brush dialog: use the requestAnimationFrame polyfill from src/compat.js.

--- brush.js
+++ brush.js
@@ -27,38 +27,8 @@ var rgbastring = function(color) {
     return "rgba("+Math.round(color.red)+","+Math.round(color.green)+","+Math.round(color.blue)+","+(color.alpha/255)+")";
 };
 
-// robust poly fill for window.requestAnimationFrame
-if (typeof window !== 'undefined') {
-        (function() {
-            var lastTime = 0;
-            var vendors = ['ms', 'moz', 'webkit', 'o'];
-            var x;
-            for(x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-                window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-                window.cancelAnimationFrame =
-                    window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-            }
-
-            if (!window.requestAnimationFrame) {
-                console.log("Using requestAnimationFrame fallback.");
-                window.requestAnimationFrame = function(callback, element) {
-                    var currTime = new Date().getTime();
-                    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                    var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                                               timeToCall);
-                    lastTime = currTime + timeToCall;
-                    return id;
-                };
-            }
-
-            if (!window.cancelAnimationFrame) {
-                window.cancelAnimationFrame = function(id) {
-                    clearTimeout(id);
-                };
-            }
-        })();
-}
-
+/* Memoize update function so that it is invoked later in a
+ * requestAnimationFrame callback. */
 var onAnim = (function() {
   var funcs = {};
   var lastArgs = {};
@@ -77,7 +47,7 @@ var onAnim = (function() {
     return function() {
       lastArgs[name] = Array.prototype.slice.call(arguments);
       if (animateScheduled) { return; }
-      window.requestAnimationFrame(animate);
+      Compat.requestAnimationFrame(animate);
       animateScheduled = true;
     };
   };
