9d6db8a969952fa43153c7b0cbd2fdfb50211246
C. Scott Ananian <cscott@cscott.net>
2012-10-03 18:40:00 -0400

    Factor out the colorwheel widget from the brush dialog.

--- brush.js
+++ brush.js
@@ -16,7 +16,7 @@ require.config({
         text: "../plugins/text"
     }
 });
-define(['domReady!', './src/color', './src/compat', './src/coords', './src/dom', './src/iscroll', './src/slider'], function(document, Color, Compat, Coords, Dom, iScroll, Slider) {
+define(['domReady!', './src/color', './src/colorwheel', './src/compat', './src/iscroll', './src/slider'], function(document, Color, ColorWheel, Compat, iScroll, Slider) {
     'use strict';
 
 
@@ -53,101 +53,85 @@ var onAnim = (function() {
   };
 })();
 
-var pol2xy = function(theta, r, size) {
-  if (!size) {
-    size = document.querySelector('#brushpane .wheel > .color').clientWidth;
-  }
-  var theta_rad = 2*Math.PI*theta/256;
-  var r_scaled = r*(size/2)/255;
-  var x = Math.cos(theta_rad) * r_scaled;
-  var y = -Math.sin(theta_rad) * r_scaled;
-  return [x + (size/2), y + (size/2)];
+var colorFromInputs = function(opt_prefix) {
+    var prefix = opt_prefix || 'color_';
+    var hlsa = ['hue','lightness','saturation','opacity'].map(function(id) {
+        return parseInt(document.getElementById(prefix+id).value, 10) || 0;
+    });
+    return { h: hlsa[0], l: hlsa[1], s: hlsa[2], a: hlsa[3] };
 };
-var _drawWheel = onAnim('_drawWheel', function(lightness) {
-  var color = document.querySelector('#brushpane .wheel > .color');
-  var white = color.querySelector('.white');
-  var black = color.querySelector('.black');
-  lightness /= 255;
-  if (lightness < 0.5) {
-    white.style.opacity = 0;
-    black.style.opacity = 1 - (lightness*2);
-  } else {
-    white.style.opacity = (lightness*2)-1;
-    black.style.opacity = 0;
-  }
-});
-var drawWheel = (function() {
-  var lastLightness = null;
-  return function() {
-    var lightness = parseInt(document.getElementById('color_lightness').value,
-                             10) || 0;
-    if (lightness === lastLightness) { return; }
-    lastLightness = lightness;
-    _drawWheel(lightness);
-  };
-})();
-var updateColorFromInputs = window.updateColorFromInputs = function() {
-  var hsla = ["color_hue", "color_saturation", "color_lightness", "brush_opacity"].map(function(id) {
-    return parseInt(document.getElementById(id).value, 10) || 0;
-  });
-  updateColor.apply(null, hsla);
-  drawWheel();
-};
-function updateOldColorFromInputs() {
-  var hsla = ["color_hue", "color_saturation", "color_lightness", "brush_opacity"].map(function(id) {
-    return parseInt(document.getElementById('old_'+id).value, 10) || 0;
-  });
-  updateOldColor.apply(null, hsla);
-}
-function updateOldColor(hue, saturation, lightness, opacity) {
-  var color = Color.from_hls(hue*360/256, lightness/255, saturation/255, opacity);
-  ['.swatches > .old'].forEach(function(sel) {
-      var e = document.querySelector('#brushpane '+sel);
-      e.style.color = rgbastring(color);
-  });
-}
-/* hue=[0,255], sat=[0,255], lightness=[0,255], opacity=[0,255] */
-var updateColor = onAnim('updateColor', function(hue, saturation, lightness, opacity) {
-  var color = Color.from_hls(hue*360/256, lightness/255, saturation/255, opacity);
-  ['.color_select > span','.opacity_options .fd-slider-bar','.wheel > .thumb'].forEach(function(sel) {
-      var e = document.querySelector('#brushpane '+sel);
-      e.style.color = rgbstring(color);
-  });
-  ['.swatches > .new'].forEach(function(sel) {
-      var e = document.querySelector('#brushpane '+sel);
-      e.style.color = rgbastring(color);
-  });
-  // saturated version of color
-  ['.lightness .fd-slider-inner'].forEach(function(sel) {
-      var e = document.querySelector('#brushpane '+sel);
-      e.style.color = rgbstring(Color.from_hls(hue*360/256, 0.5, 1));
-  });
-});
+
 function pageLoaded() {
   var brushpane = document.querySelector("#brushpane");
   var PAGES = ['color', 'brush'];
   PAGES.forEach(function(p) {
-    var e = document.querySelector("#brushpane ."+p+"_select");
+    var e = brushpane.querySelector("."+p+"_select");
     e.addEventListener('click', function(event) {
        PAGES.forEach(function(pp) { brushpane.classList.remove(pp); });
        brushpane.classList.add(p);
        event.preventDefault(); /* don't change history */
     }, false);
   });
-  document.querySelector("#brushpane .closer").addEventListener("click",
+  brushpane.querySelector(".closer").addEventListener("click",
     function(event) {
        brushpane.classList.remove('visible');
        event.preventDefault(); /* don't change history */
     }, false);
   ['size','spacing'].forEach(function(type) {
     ['minus','plus'].forEach(function(dir) {
-       var e = document.querySelector('#brushpane .'+type+' .'+dir);
+       var e = brushpane.querySelector('.'+type+' .'+dir);
        e.addEventListener("click", function(event) {
          Slider.increment('brush_'+type, (dir==='minus')?-1:1);
          event.preventDefault();
        });
     });
   });
+
+  var updateOldColor = function(hue, saturation, lightness, opacity) {
+      var color = Color.from_hls(hue*360/256, lightness/255, saturation/255, opacity);
+      ['.swatches > .old'].forEach(function(sel) {
+          var e = brushpane.querySelector(sel);
+          e.style.color = rgbastring(color);
+      });
+  };
+  /* hue=[0,255], sat=[0,255], lightness=[0,255], opacity=[0,255] */
+  var updateColor = onAnim('updateColor', function(hue, saturation, lightness, opacity) {
+      var color = Color.from_hls(hue*360/256, lightness/255, saturation/255, opacity);
+      ['.color_select > span','.opacity_options .fd-slider-bar'].forEach(function(sel) {
+          var e = brushpane.querySelector(sel);
+          e.style.color = rgbstring(color);
+      });
+      ['.swatches > .new'].forEach(function(sel) {
+          var e = brushpane.querySelector(sel);
+          e.style.color = rgbastring(color);
+      });
+      // saturated version of color
+      ['.lightness .fd-slider-inner'].forEach(function(sel) {
+          var e = brushpane.querySelector(sel);
+          e.style.color = rgbstring(Color.from_hls(hue*360/256, 0.5, 1));
+      });
+  });
+
+  var wheel = new ColorWheel(brushpane.querySelector('.wheel'), brushpane);
+  wheel.hsCallback = function(hue, saturation) {
+      var c = colorFromInputs();
+      document.getElementById('color_hue').value = hue;
+      document.getElementById('color_saturation').value = saturation;
+      updateColor(hue, saturation, c.l, c.a);
+  };
+  var wheel_setHSL = onAnim('wheel_setHSL', function(h, s, l) {
+      wheel.setHSL(h, s, l);
+  });
+  var updateColorFromInputs = window.updateColorFromInputs = function() {
+      var c = colorFromInputs();
+      updateColor(c.h, c.s, c.l, c.a);
+      wheel_setHSL(c.h, c.s, c.l);
+  };
+  var updateOldColorFromInputs = function() {
+      var c = colorFromInputs('old_color_');
+      updateOldColor(c.h, c.s, c.l, c.a);
+  };
+
   var setLightness = function(l) {
       document.getElementById('color_lightness').value=Math.round(l);
       Slider.updateSlider('color_lightness');
@@ -155,150 +139,28 @@ function pageLoaded() {
   };
   var setHSLA = function(h, s, l, a) {
     [['color_hue', h], ['color_saturation', s],
-     ['color_lightness', l], ['brush_opacity', a]].forEach(function(a) {
+     ['color_lightness', l], ['color_opacity', a]].forEach(function(a) {
       document.getElementById(a[0]).value=a[1];
       Slider.updateSlider(a[0]);
     });
-    updateThumb();
     updateColorFromInputs();
   };
   ['white','black','old'].forEach(function(sw) {
-    var e = document.querySelector('#brushpane .swatches > .'+sw);
+    var e = brushpane.querySelector('.swatches > .'+sw);
     e.addEventListener("click", function(event) {
         if (sw==='white') {
            setLightness(255);
         } else if (sw==='black') {
            setLightness(0);
         } else {
-           var oldhsla = ['color_hue', 'color_saturation',
-                          'color_lightness', 'brush_opacity'].map(function(id){
-             return parseInt(document.getElementById('old_'+id).value, 10) || 0;
-           });
-           setHSLA.apply(null, oldhsla);
+           var old = colorFromInputs('old_color_');
+           setHSLA(old.h, old.s, old.l, old.a);
         }
         event.preventDefault();
     });
   });
-  var wheel = document.querySelector('#brushpane .wheel > .color');
-  var updateThumbFromEvent = function(event) {
-    if (event.touches) { // synthesize new event w/ first touch
-      if (event.touches.length===0) { return; }
-      // synthesize new event with data from first touch
-      event = {
-        type: event.type,
-        pageX: event.touches[0].pageX,
-        pageY: event.touches[0].pageY,
-        clientX: event.touches[0].clientX,
-        clientY: event.touches[0].clientY,
-        target: event.touches[0].target || event.target || event.srcElement
-      };
-    }
-    var coords = Coords.getRelativeEventPosition(event, wheel);
-    var ox = coords.x, oy = coords.y;
-    var x = (ox - (wheel.offsetWidth/2)) / (wheel.clientHeight/2);
-    var y = (oy - (wheel.offsetHeight/2)) / (wheel.clientHeight/2);
-    var theta = Math.atan2(-y,x)/(2*Math.PI), r = Math.sqrt(x*x + y*y);
-    if (theta < 0) { theta += 1; }
-    if (r > 1) { ox = oy = null; }
-    // set hue/sat
-    var h = Math.min(theta*256, 255), s = Math.min(r*255, 255);
-    document.getElementById('color_hue').value=Math.round(h);
-    document.getElementById('color_saturation').value=Math.round(s);
-    updateThumb(h, s, ox, oy);
-    updateColorFromInputs();
-  };
-  var updateThumb = onAnim('updateThumb', function(h, s, x, y) {
-    if (typeof(h)!=='number') {
-      h = parseInt(document.getElementById('color_hue').value, 10) || 0;
-      s = parseInt(document.getElementById('color_saturation').value, 10) || 0;
-    }
-    if (typeof(x)!=='number') {
-      var xy = pol2xy(h, s);
-      x = xy[0], y = xy[1];
-    }
-    var thumb = document.querySelector('#brushpane .wheel > .thumb');
-    var transform = Math.round(x)+'px,'+Math.round(y)+'px';
-    // the '3d' is actually very important here: it enables
-    // GPU acceleration of this transform on webkit
-    thumb.style.WebkitTransform =
-            'translate3d('+transform+',0)';
-    thumb.style.MozTransform = thumb.style.transform =
-            'translate('+transform;+')';
-  });
-  var handleMouseDown = function(event) {
-    updateThumbFromEvent(event);
-    if (event.type==='mousedown') {
-      brushpane.addEventListener('mousemove', handleMouseMove, true);
-      brushpane.addEventListener('mouseup', handleMouseUp, true);
-      brushpane.addEventListener('mouseout', handleMouseOut, true);
-    } else {
-      brushpane.addEventListener('touchmove', handleMouseMove, true);
-      brushpane.addEventListener('touchend', handleMouseUp, true);
-      brushpane.addEventListener('touchcancel', handleMouseUp, true);
-      brushpane.addEventListener('touchleave', handleMouseUp, true);
-      event.preventDefault();
-    }
-  }
-  var handleMouseMove = function(event) {
-    updateThumbFromEvent(event);
-  }
-  var handleMouseOut = function(event) {
-    var related = event.relatedTarget, target = this;
-    // For mousenter/leave call the handler if related is outside the target.
-    // NB: No relatedTarget if the mouse left/entered the browser window
-    if (!related || (related !== target && !target.contains(related))) {
-      // emualate mouseleave event
-      handleMouseUp(event);
-    }
-  };
-  var handleMouseUp = function(event) {
-    updateThumbFromEvent(event);
-    if (event.type==='mouseup' || event.type==='mouseout') {
-      brushpane.removeEventListener('mousemove', handleMouseMove, true);
-      brushpane.removeEventListener('mouseup', handleMouseUp, true);
-      brushpane.removeEventListener('mouseout', handleMouseOut, true);
-    } else {
-      brushpane.removeEventListener('touchmove', handleMouseMove, true);
-      brushpane.removeEventListener('touchend', handleMouseUp, true);
-      brushpane.removeEventListener('touchcancel', handleMouseUp, true);
-      brushpane.removeEventListener('touchleave', handleMouseUp, true);
-    }
-  };
-  wheel.addEventListener('mousedown', handleMouseDown, true);
-  wheel.addEventListener('touchstart', handleMouseDown, true);
-  var handleWheelKey = function(e) {
-    var INCR = 2;
-    e = e || document.parentWindow.event;
-    var kc = e.keyCode != null ? e.keyCode : e.charCode;
-    var h = parseInt(document.getElementById('color_hue').value, 10) || 0;
-    var s = parseInt(document.getElementById('color_saturation').value, 10)|| 0;
-    switch (kc) {
-    case 37: /* left */
-      h-= INCR; break;
-    case 39: /* right */
-      h+= INCR; break;
-    case 38: /* up */
-    case 33: /* page up */
-      s+= INCR; break;
-    case 40: /* down */
-    case 34: /* page down */
-      s-= INCR; break;
-    default:
-      return true;
-    }
-    if (h < 0) h = 255; if (h > 255) h = 0;
-    if (s < 0) s = 0;   if (s > 255) s = 255;
-    document.getElementById('color_hue').value=Math.round(h);
-    document.getElementById('color_saturation').value=Math.round(s);
-    updateThumb();
-    updateColorFromInputs();
-    e.stopPropagation(); e.preventDefault();
-    return false;
-  };
-  wheel.addEventListener('keypress', handleWheelKey, true);
 
   Slider.create();
-  updateThumb();
   updateColorFromInputs();
   updateOldColorFromInputs();
 
---
+++ src/colorwheel.js
@@ -0,0 +1,195 @@
+/*jshint
+  eqeqeq:true, curly:true, latedef:true, newcap:true, undef:true,
+  trailing:true, es5:true, globalstrict:true
+ */
+/*global define:false, console:false, document:false, window:false */
+define(['./color','./coords'], function(Color, Coords) {
+    'use strict';
+    /** HSL Color wheel widget.
+     */
+    var ColorWheel = function(domElement, containerElement) {
+        this.domElement = domElement;
+        this.container = containerElement || domElement;
+        this.hue = this.saturation = this.lightness = null;
+        this.setHSL(0,0,0);
+        // add event handlers.
+        var addEvent = function(type, func, elem) {
+            (elem || this.container).addEventListener(type, func, true);
+        }.bind(this);
+        var removeEvent = function(type, func, elem) {
+            (elem || this.container).removeEventListener(type, func, true);
+        }.bind(this);
+        var handleMouseDown, handleMouseMove, handleMouseUp, handleMouseOut;
+        handleMouseDown = function(event) {
+            this._updateThumbFromEvent(event);
+            if (event.type==='mousedown') {
+                addEvent('mousemove', handleMouseMove);
+                addEvent('mouseup', handleMouseUp);
+                addEvent('mouseout', handleMouseOut);
+            } else {
+                addEvent('touchmove', handleMouseMove);
+                addEvent('touchend', handleMouseUp);
+                addEvent('touchcancel', handleMouseUp);
+                addEvent('touchleave', handleMouseUp);
+                event.preventDefault();
+            }
+        }.bind(this);
+        handleMouseMove = function(event) {
+            this._updateThumbFromEvent(event);
+        }.bind(this);
+        handleMouseOut = function(event) {
+            var related = event.relatedTarget, target = this;
+            // For mousenter/leave call the handler if related is outside the
+            // target.
+            // NB: No relatedTarget if the mouse left/entered the browser window
+            if (!related || (related !== target && !target.contains(related))) {
+                // emualate mouseleave event
+                handleMouseUp(event);
+            }
+        };
+        handleMouseUp = function(event) {
+            this._updateThumbFromEvent(event);
+            if (event.type==='mouseup' || event.type==='mouseout') {
+                removeEvent('mousemove', handleMouseMove);
+                removeEvent('mouseup', handleMouseUp);
+                removeEvent('mouseout', handleMouseOut);
+            } else {
+                removeEvent('touchmove', handleMouseMove);
+                removeEvent('touchend', handleMouseUp);
+                removeEvent('touchcancel', handleMouseUp);
+                removeEvent('touchleave', handleMouseUp);
+            }
+        }.bind(this);
+        addEvent('mousedown', handleMouseDown, this.domElement);
+        addEvent('touchstart', handleMouseDown, this.domElement);
+
+        var handleWheelKey = function(e) {
+            var INCR = 2;
+            e = e || document.parentWindow.event;
+            var kc = e.keyCode != null ? e.keyCode : e.charCode;
+            var h = this.hue;
+            var s = this.saturation;
+            switch (kc) {
+            case 37: /* left */
+                h-= INCR; break;
+            case 39: /* right */
+                h+= INCR; break;
+            case 38: /* up */
+            case 33: /* page up */
+                s+= INCR; break;
+            case 40: /* down */
+            case 34: /* page down */
+                s-= INCR; break;
+            default:
+                return true;
+            }
+            e.stopPropagation(); e.preventDefault();
+            if (h < 0) { h = 255; }
+            if (h > 255) { h = 0; }
+            s = Math.max(0, Math.min(s, 255));
+            this.hue = h;
+            this.saturation = s;
+            this._updateThumbPosition();
+            this._updateThumbColor();
+            this.hsCallback(h,s); // let clients know hue/sat has been updated.
+            return false;
+        };
+        addEvent('keypress', handleWheelKey, this.domElement);
+    };
+    ColorWheel.prototype = {};
+    ColorWheel.prototype._pol2xy = function(theta, r, size) {
+        if (!size) {
+            size = this.domElement.querySelector('.color').clientWidth;
+        }
+        var theta_rad = 2*Math.PI*theta/256;
+        var r_scaled = r*(size/2)/255;
+        var x = Math.cos(theta_rad) * r_scaled;
+        var y = -Math.sin(theta_rad) * r_scaled;
+        return { x: x + (size/2), y: y + (size/2) };
+    };
+    ColorWheel.prototype._updateThumbFromEvent = function(event) {
+        if (event.touches) { // synthesize new event w/ first touch
+            if (event.touches.length===0) { return; }
+            // synthesize new event with data from first touch
+            event = {
+                type: event.type,
+                pageX: event.touches[0].pageX,
+                pageY: event.touches[0].pageY,
+                clientX: event.touches[0].clientX,
+                clientY: event.touches[0].clientY,
+                target: event.touches[0].target || event.target ||
+                    event.srcElement
+            };
+        }
+        var inner = this.domElement.querySelector('.color');
+        var coords = Coords.getRelativeEventPosition(event, inner);
+        var ox = coords.x, oy = coords.y;
+        var x = (ox - (inner.offsetWidth/2)) / (inner.clientHeight/2);
+        var y = (oy - (inner.offsetHeight/2)) / (inner.clientHeight/2);
+        var theta = Math.atan2(-y,x)/(2*Math.PI), r = Math.sqrt(x*x + y*y);
+        if (theta < 0) { theta += 1; }
+        if (r > 1) { ox = oy = null; }
+        // set hue/sat
+        this.hue = Math.round(Math.min(theta*256, 255));
+        this.saturation = Math.round(Math.min(r*255, 255));
+        this._updateThumbPosition(ox, oy);
+        this._updateThumbColor();
+        // let clients know this has been updated.
+        this.hsCallback(this.hue,this.saturation);
+    };
+    ColorWheel.prototype._updateThumbPosition = function(x, y) {
+        var pos = { x: x, y: y };
+        if (typeof(pos.x)!=='number') {
+            pos = this._pol2xy(this.hue, this.saturation);
+        }
+        var thumb = this.domElement.querySelector('.thumb');
+        var transform = Math.round(pos.x)+'px,'+Math.round(pos.y)+'px';
+        // the '3d' is actually very important here: it enables
+        // GPU acceleration of this transform on webkit
+        thumb.style.WebkitTransform =
+            'translate3d('+transform+',0)';
+        thumb.style.MozTransform = thumb.style.transform =
+            'translate('+transform+')';
+    };
+    ColorWheel.prototype._updateThumbColor = function() {
+        var thumb = this.domElement.querySelector('.thumb');
+        var color = Color.from_hls(this.hue*360/256, this.lightness/255,
+                                   this.saturation/255, 255);
+        thumb.style.color = color.to_string().substring(0, 7);
+    };
+    ColorWheel.prototype.setHSL = function(h, s, l) {
+        var hsChanged = (this.hue!==h || this.saturation!==s);
+        var lChanged = (this.lightness!==l);
+        if (hsChanged) {
+            this.hue = h;
+            this.saturation = s;
+            this._updateThumbPosition();
+        }
+        if (lChanged) {
+            this.setLightness(l); // implicitly calls _updateThumbColor()
+        } else if (hsChanged) {
+            this._updateThumbColor();
+        }
+    };
+    ColorWheel.prototype.setLightness = function(lightness/* [0,255] */) {
+        if (lightness === this.lightness) { return; }
+        this.lightness = lightness;
+        var white = this.domElement.querySelector('.white');
+        var black = this.domElement.querySelector('.black');
+        lightness /= 255;
+        if (lightness < 0.5) {
+            white.style.opacity = 0;
+            black.style.opacity = 1 - (lightness*2);
+        } else {
+            white.style.opacity = (lightness*2)-1;
+            black.style.opacity = 0;
+        }
+        this._updateThumbColor();
+    };
+    ColorWheel.prototype.hsCallback = function(hue, saturation) {
+        /* override to receive updates */
+    };
+
+
+    return ColorWheel;
+});
