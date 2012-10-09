7386cfe5f87e04e1bfc57803d91b41ef3530fa76
C. Scott Ananian <cscott@cscott.net>
2012-10-09 15:11:50 -0400

    Make print-version script less dependent on node modules; fix webapp manifest.

--- bin/print-version.js
+++ bin/print-version.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 // node script to emit the current version of the source code
 
-var requirejs = require('requirejs');
-requirejs(['commander', 'fs', '../src/version'], function(program, fs, version){
-    program
-        .version(version)
-        .usage('')
-        .parse(process.argv);
+var requirejs = require('../r.js');
+requirejs(['../src/version'], function(version) {
+
+    try {
+        // use the node module 'commander' to give a nicer interface,
+        // if installed.
+        var program = require('commander');
+        program
+            .version(version)
+            .usage('')
+            .parse(process.argv);
+    } catch (e) { /* I guess it's not installed */ }
 
     console.log(version);
 });
